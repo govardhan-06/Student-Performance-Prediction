622e6e765e1d38057c65e5f3f43f5f54656d5d1b
govardhan-06 <156232211+govardhan-06@users.noreply.github.com>
2024-06-20 07:02:01 +0530

    Model update

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 
 def error_message_detail(error, error_detail:sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -20,11 +21,3 @@ class customException(Exception):
     # __str__ is to print() the value
     def __str__(self):
         return self.error_message
-
-if __name__=="__main__":
-    try:
-        a=1
-        print(a/0)
-    except Exception as e:
-        print("Divison by zero")
-        raise customException(e, sys)
